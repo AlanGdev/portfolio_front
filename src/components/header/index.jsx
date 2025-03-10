7c9805c39cb1471b28a87b6ce1e0dcf757d717f7
Alan.Grolleau <alan.grolleau@gmail.com>
2025-03-10 22:02:50 +0100

    Bouton header à droite

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,11 +3,10 @@ import { Container, Button } from 'react-bootstrap';
 function Header({ darkMode, setDarkMode }) {
   return (
     <header className={darkMode ? 'bg-dark text-light' : 'bg-light text-dark'}>
-      <Container className="text-center pt-3">
+      <Container className="pt-1 d-flex justify-content-end">
         <Button
           onClick={() => setDarkMode(!darkMode)}
           variant={darkMode ? 'outline-light' : 'outline-dark'}
-          className="w-100"
         >
           {darkMode ? 'Passer en Mode Clair' : 'Passer en Mode Sombre'}
         </Button>
